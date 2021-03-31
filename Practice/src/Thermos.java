abdcc3992672a111765e6a25797369a438c2111a
sysadmin <sysadmin@bcalab1-m13>
2021-03-31 09:36:06 +0530

    # WARNING: head commit changed in the meantime
    First commit

---
+++ Practice/src/Thermos.java
@@ -0,0 +1,39 @@
+
+public class Thermos {
+	
+	float Qty = 0;
+	String Brand;
+	String Liquid;
+	
+	Thermos(float Qty, String Brand,String Liquid )
+	{
+		this.Qty=Qty;
+		this.Brand=Brand;
+		this.Liquid=Liquid;
+	}
+	
+	void addLiquid(double Q)
+	{
+			if(Q <= 1) {
+				Qty = (float) (Qty + Q);
+			}else {
+				System.out.print("The capacity of thermos is 1 litre");
+			}
+		}
+		
+		void displayDetails() {
+			System.out.print("\nBrand: "+Brand);
+			System.out.print("\nLiquid: "+Liquid);
+			System.out.print("\nVolume(in Litres): "+Qty);
+		}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thermos Godrej = new Thermos(0, "Godrej", "Orange Juice");
+		Godrej.addLiquid(0.8  );
+		Godrej.displayDetails();
+	}
+
+}
+
+
